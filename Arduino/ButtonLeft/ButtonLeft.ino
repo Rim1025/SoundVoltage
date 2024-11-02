d2b83a6d3bdb072c9b914dd66e6498c1cbc5e197
Rim Kakino <rimu1025@icloud.com>
2024-11-02 16:28:18 +0900

    Arduinoファイルを追加

---
+++ Arduino/ButtonLeft/ButtonLeft.ino
@@ -0,0 +1,17 @@
+bool state7 = 0;
+bool state8 = 0;
+bool state9 = 0;
+void setup()
+{
+  Serial.begin(9600);
+
+  DDRB = 0b00000000;//ポートBを全てINPUT
+  DDRD = 0b00000000;
+}
+void loop() {
+  state7 = (PIND>>7) & 0b00000001;
+  state8 = PINB & 0b00000001;
+  state9 = (PINB>>1) & 0b00000001;
+  Serial.println(((state7<<3)|(state8<<2)|(state9<<1)|1));
+  delay(10);
+}
